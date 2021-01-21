c88f9d92f34ab6e2422fb11e9a4ec70e2772de54
Violette Jacobs <violettejacobs@MacBook-Pro-de-Violette.local>
2021-01-21 11:45:38 +0100

    structure de base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+// import bootstrap/jquery
+import "bootstrap/dist/css/bootstrap.min.css"
+import "jquery/dist/jquery"
+import "bootstrap/dist/js/bootstrap.bundle.min"
+// import fontawesome
+import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import components
 
+
+import react,  {Component, Fragment} from "react"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Fragment>
+
+    </Fragment>
   );
 }
 
